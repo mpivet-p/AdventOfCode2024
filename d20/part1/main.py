65c23d356ee7707a48a9e784807a15622793ba01
Maxime Pivet-Pierre <mpivet-p@student.42.fr>
2024-12-21 15:18:37 +0800

    d20 part 1 done

--- d20/part1/main.py
+++ d20/part1/main.py
@@ -3,44 +3,57 @@ import sys
 
 costs = {}
 solutions = []
+dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 def solve(map, pos, visited):
+  visited[tuple(pos)] = len(visited)
+
   if map[tuple(pos)] == "E":
     solutions.append(visited)
     return True
   
   solved = False
 
-  next_pos = [(0, 1), (1, 0), (0, -1), (-1, 0)] + pos
+  next_pos = dirs + pos
   for nc in next_pos:
-    if np.all(nc >= 0) and np.all(nc < len(map)):
+    if np.all(nc >= 1) and np.all(nc < len(map) - 1):
       nc_t = tuple(nc)
-      if  nc_t not in visited and map[nc_t] != "#" and (nc_t not in costs or len(visited) < costs[nc_t]):
-        costs[nc_t] = len(visited) + 1
-        visited.add(nc_t)
+      if nc_t not in set(visited) and map[nc_t] != "#":
         if solve(map, nc, visited.copy()):
-          return True
-          # solved = True
-
+          solved = True
   return solved
 
+def cheats(map, solution):
+  possible_cheats = []
+  for i in range(1, len(map)- 1):
+    for j in range(1, len(map) -1):
+      if map[i, j] == "#":
+        pp = dirs + np.array((i, j))
+        pp = [tuple(pp[0]), tuple(pp[1]), tuple(pp[2]), tuple(pp[3])]
+        if pp[0] in solution and pp[2] in solution:
+          possible_cheats.append(((max(solution[pp[0]], solution[pp[2]]) - min(solution[pp[0]], solution[pp[2]])) - 2, (i, j)))
+        if pp[1] in solution and pp[3] in solution:
+          possible_cheats.append(((max(solution[pp[1]], solution[pp[3]]) - min(solution[pp[1]], solution[pp[3]])) - 2, (i, j)))
+
+  return possible_cheats
+
+
 def main():
-  # sys.setrecursionlimit(10000)
+  sys.setrecursionlimit(10000)
 
-  with open("test-input.txt", "r") as file:
-    map = []
+  with open("input.txt", "r") as file:
+    map_ = []
     for line in file:
-      map.append(list(line.strip()))
-    map = np.array(map)
+      map_.append(list(line.strip()))
+    map_ = np.array(map_)
 
-  start = np.argwhere(map == "S")[0]
-  end = np.argwhere(map == "E")[0]
+  start = np.argwhere(map_ == "S")[0]
 
-  solve(map, start, set())
+  solve(map_, start, {})
+  possible_cheats = cheats(map_, solutions[0])
 
-  print(len(solutions), len(sorted(solutions)[0]) - 1)
-  # print(len(solutions) - 1, solutions[0])
-
+  possible_cheats = [n[0] for n in possible_cheats]
+  print(sum([1 if pc >= 100 else 0 for pc in possible_cheats]))
 
 if __name__ == "__main__":
   main()
